df7c7c6824c4804e29ce266b375ab5dd6566c235
jayarani.ys <jayaraniys1@gmail.com>
2025-03-26 14:08:55 +0530

    new features added

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -5,11 +5,15 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "./InfoBox.css";
+import { Construction } from '@mui/icons-material';
 
 export default function InfoBox({info}){
     const INIT_URL=
     "https://images.unsplash.com/photo-1622278647429-71bc97e904e8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   
+    const  HOT_URL="https://images.unsplash.com/photo-1561647784-2f9c43b07a0b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const RAIN_URL="https://plus.unsplash.com/premium_photo-1671229652411-4468b946b787?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL="https://plus.unsplash.com/premium_photo-1670604649107-a0171e5f1bd0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     return <div className='InfoBox'>
         {/* <h1>weather information={info.weather}</h1> */}
       <div className="cardcontainer">
@@ -17,12 +21,12 @@ export default function InfoBox({info}){
     <Card sx={{ maxWidth: 345 }}>
     <CardMedia
       sx={{ height: 140 }}
-      image={INIT_URL}
+      image={info.humidity>80 ? RAIN_URL: (info.temp>15) ? HOT_URL : COLD_URL}
       title="green iguana"
     />
     <CardContent>
       <Typography gutterBottom variant="h5" component="div">
-    <div>city={info.city}</div>
+    <div>{info.city}</div>
       </Typography>
       <Typography variant="body2" sx={{ color: 'text.secondary' }}>
         <div>Temperature={info.temp}&deg;C</div>
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,41 +3,49 @@ import Button from '@mui/material/Button';
 import "./SearchBox.css";
 import { useState } from 'react';
 export default function SearchBox({updateInfo}){
+  let[error,setError]=useState(false);
     let[city,setCity]= useState("");
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY="f8ccd7596ddc4bc08ad6ab8105bae691";
 
     let getWeatherInfo= async() =>{
-       let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
-       let jsonResponse= await response.json();
-       console.log(jsonResponse);
-       let result={
-        city:city,
+      try{ let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+      let jsonResponse= await response.json();
+      console.log(jsonResponse);
+      let result={
+       city:city,
 
-        temp:jsonResponse.main.temp,
-        minTemp:jsonResponse.main.temp_min,
-        maxTemp:jsonResponse.main.temp_max,
-        humidity:jsonResponse.main.humidity,
-        feelsLike:jsonResponse.main.feels_like,
-        weather:jsonResponse.weather[0].description
-       }
-       console.log(result);
-       return result;
-    };
+       temp:jsonResponse.main.temp,
+       minTemp:jsonResponse.main.temp_min,
+       maxTemp:jsonResponse.main.temp_max,
+       humidity:jsonResponse.main.humidity,
+       feelsLike:jsonResponse.main.feels_like,
+       weather:jsonResponse.weather[0].description
+      }
+      console.log(result);
+      return result;
+   }catch (error){
+    throw error;
+   }
+   };
     
-    
-    let handleChange=(event)=>{
-      setCity(event.target.value);
-    };
-    let handleSubmit=async(event)=>
-    {
-        event.preventDefault();
-        console.log(city);
-        setCity("");
-       let newInfo= await getWeatherInfo();
-       updateInfo(newInfo);
-       
-    };
+   let handleChange=(event)=>{
+    setCity(event.target.value);
+  };
+
+
+  let handleSubmit=async(event)=>
+  {
+    try{
+      event.preventDefault();
+      console.log(city);
+      setCity("");
+     let newInfo= await getWeatherInfo();
+     updateInfo(newInfo);}
+     catch(error){
+      setError(true);
+     }
+         };
     return(<div className="searchBox">
     <form onSubmit={handleSubmit}>
     <TextField id="city" label="City Name" variant="outlined" required value={city} 
@@ -46,6 +54,7 @@ export default function SearchBox({updateInfo}){
     <Button variant="contained" type="submit" >
         Search
       </Button>
+      {error && <p>No such place exists in our API</p>}
     </form>
     </div>);
 }
